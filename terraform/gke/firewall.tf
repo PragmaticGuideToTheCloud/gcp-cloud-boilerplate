1a903341352a2d89d6f31fa469bb6d6c40d2675a
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2021-03-26 19:40:30 +0100

    Revert "fix: use tfstate configuration for gke from parent dir"
    This reverts commit 9d6ad57d864faea4b92e06f4e85378c26087adc0.

--- terraform/gke/firewall.tf
+++ terraform/gke/firewall.tf
@@ -1,9 +1,9 @@
 resource "google_compute_firewall" "gke" {
-  depends_on = [ data.remote_state.vpc ]
+  depends_on = [ data.terraform_remote_state.vpc ]
 
   name = var.env_name
 
-  network = data.remote_state.vpc.outputs.vpc_link
+  network = data.terraform_remote_state.vpc.outputs.vpc_link
 
   allow {
     protocol = "all"
