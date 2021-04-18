808207efb7d1ecfac43a0f0809c4f71510f876da
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2021-04-18 11:37:08 +0200

    feat: default values for database

--- terraform/sql/variables.tf
+++ terraform/sql/variables.tf
@@ -32,28 +32,32 @@ variable "deletion_protection" {
   default = false
 }
 
-variable "database_version" {
-  type = string
-  default = "POSTGRES_13"
-}
-
 variable "database_tier" {
   type = string
   default = "db-f1-micro"
 }
 
+variable "database_version" {
+  type = string
+  default = "POSTGRES_13"
+}
+
 variable "database_root_password" {
   type = string
+  default = "thisIsA123PASS"
 }
 
 variable "database_user" {
   type = string
+  default = "apiuser"
 }
 
 variable "database_password" {
   type = string
+  default = "SecretPassword123!"
 }
 
 variable "database_name" {
   type = string
+  default = "apidb"
 }
