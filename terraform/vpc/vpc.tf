37e848cbda6fd6455f709fc472d915aede767ec5
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2021-06-26 12:46:59 +0200

    without NAT

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -2,14 +2,14 @@ resource "google_compute_network" "vpc" {
   name = var.vpc_name
 }
 
-resource "google_compute_router" "vpc" {
-  name = "${var.vpc_name}-router"
-  network = google_compute_network.vpc.self_link
-}
-
-resource "google_compute_router_nat" "vpc" {
-  name = "${var.vpc_name}-router-nat"
-  router = google_compute_router.vpc.name
-  nat_ip_allocate_option = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-}
+//resource "google_compute_router" "vpc" {
+//  name = "${var.vpc_name}-router"
+//  network = google_compute_network.vpc.self_link
+//}
+//
+//resource "google_compute_router_nat" "vpc" {
+//  name = "${var.vpc_name}-router-nat"
+//  router = google_compute_router.vpc.name
+//  nat_ip_allocate_option = "AUTO_ONLY"
+//  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+//}
