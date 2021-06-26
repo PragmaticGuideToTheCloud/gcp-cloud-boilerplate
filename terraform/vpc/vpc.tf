d2c39bac0d6147c3961a3a32185f0c88d281689c
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2021-06-26 12:44:11 +0200

    cleanup

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -1,29 +1,15 @@
 resource "google_compute_network" "vpc" {
   name = var.vpc_name
-
-//  routing_mode = "REGIONAL"
-//
-//  auto_create_subnetworks = true
-//
-//  delete_default_routes_on_create = false
 }
 
 resource "google_compute_router" "vpc" {
   name = "${var.vpc_name}-router"
-
-//  region = var.region
-
   network = google_compute_network.vpc.self_link
 }
 
 resource "google_compute_router_nat" "vpc" {
   name = "${var.vpc_name}-router-nat"
-
-//  region = var.region
-
   router = google_compute_router.vpc.name
-
   nat_ip_allocate_option = "AUTO_ONLY"
-
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
