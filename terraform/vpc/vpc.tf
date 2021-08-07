a8268ef76f63f268a29103942c24bcb01e3b489a
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2021-08-07 09:52:15 +0200

    default values for network

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -1,9 +1,12 @@
 resource "google_compute_network" "vpc" {
   name = var.vpc_name
 
-  routing_mode = "REGIONAL"
+  # this is default
+  # routing_mode = "REGIONAL"
 
-  auto_create_subnetworks = true
+  # this is default
+  # auto_create_subnetworks = true
 
-  delete_default_routes_on_create = false
+  # this is default
+  # delete_default_routes_on_create = false
 }
