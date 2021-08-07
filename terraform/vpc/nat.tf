8a57ae5d4202c4c02858516c97d1520f40330977
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2021-08-07 09:41:24 +0200

    CS

--- terraform/vpc/nat.tf
+++ terraform/vpc/nat.tf
@@ -1,19 +1,14 @@
 resource "google_compute_router" "vpc" {
-  name = "${var.vpc_name}-router"
-
-  region = var.region
-
+  name    = "${var.vpc_name}-router"
+  region  = var.region
   network = google_compute_network.vpc.self_link
 }
 
 resource "google_compute_router_nat" "vpc" {
-  name = "${var.vpc_name}-router-nat"
-
+  name   = "${var.vpc_name}-router-nat"
   region = var.region
-
   router = google_compute_router.vpc.name
 
-  nat_ip_allocate_option = "AUTO_ONLY"
-
+  nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
--- terraform/vpc/snc.tf
+++ terraform/vpc/snc.tf
@@ -1,19 +1,16 @@
 resource "google_compute_global_address" "vpc" {
-  name = "${var.vpc_name}-global-address"
-
-  purpose      = "VPC_PEERING"
-  address_type = "INTERNAL"
-
-  prefix_length = var.vpc_prefix_length
+  name          = "${var.vpc_name}-global-address"
   network       = google_compute_network.vpc.self_link
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = var.vpc_prefix_length
 }
 
 resource "google_service_networking_connection" "vpc" {
+  network = google_compute_network.vpc.self_link
   service = "servicenetworking.googleapis.com"
 
   reserved_peering_ranges = [
     google_compute_global_address.vpc.name,
   ]
-
-  network = google_compute_network.vpc.self_link
 }
