0adb9819b23ee67a1c0825f77be6b0a707a16250
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2022-02-20 09:37:56 +0100

    feat: NAT attributes changed after apply

--- terraform/vpc/nat.tf
+++ terraform/vpc/nat.tf
@@ -13,4 +13,7 @@ resource "google_compute_router_nat" "vpc" {
 
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  drain_nat_ips = []
+  nat_ips = []
 }
