d851083d77ed2af3b73eb60135baae7910c60fdf
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2022-02-20 09:11:30 +0100

    cleanup

--- terraform/crt/outputs.tf
+++ terraform/crt/outputs.tf
@@ -1,10 +1,10 @@
 
 output "certificate_link" {
-  value = google_compute_ssl_certificate.aiqa-crt-gcp.self_link
+  value = google_compute_ssl_certificate.foobar-crt-gcp.self_link
 }
 
 output "certificate_id" {
-  value = google_compute_ssl_certificate.aiqa-crt-gcp.certificate_id
+  value = google_compute_ssl_certificate.foobar-crt-gcp.certificate_id
 }
 
 # vim:ts=2:sw=2:et:syn=terraform:
