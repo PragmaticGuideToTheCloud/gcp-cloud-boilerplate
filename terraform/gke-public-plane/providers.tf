180b17610f1af82dd2f3b585898b91f8a387b2c4
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2022-03-12 07:46:26 +0100

    fix terraform and provider version

--- terraform/gke-public-plane/providers.tf
+++ terraform/gke-public-plane/providers.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = "1.1.7"
+
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.11.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.8.0"
+    }
+  }
+}
+
 provider "google" {
   region  = var.region
   project = var.project
